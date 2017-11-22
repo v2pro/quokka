78e283a796c6e8fa26275a1c161e0016b2494406
Tao Wen <taowen@gmail.com>
2017-11-22 10:11:45 +0800

    add docstore

---
+++ docstore/model/model.go
@@ -0,0 +1,101 @@
+package model
+
+import (
+	"github.com/json-iterator/go"
+	"reflect"
+	"unsafe"
+	"io"
+	"fmt"
+)
+
+// unmarshal will only return following types
+// float64
+// string
+// bool
+// *DArray
+// *DObject
+
+var json = jsoniter.Config{
+	EscapeHTML:                    false,
+	MarshalFloatWith6Digits:       true, // will lose precession
+	ObjectFieldMustBeSimpleString: true, // do not unescape object field
+}.Froze()
+
+var emptyInterfaceType = reflect.TypeOf((*interface{})(nil)).Elem()
+
+func init() {
+	json.RegisterExtension(&jsonExtension{})
+}
+
+type DObject struct {
+	data map[string]interface{}
+}
+
+type DArray struct {
+	data []interface{}
+}
+
+type jsonExtension struct {
+	jsoniter.DummyExtension
+}
+
+func (extension *jsonExtension) CreateDecoder(typ reflect.Type) jsoniter.ValDecoder {
+	if typ == emptyInterfaceType {
+		return &emptyInterfaceDecoder{}
+	}
+	return nil
+}
+
+type emptyInterfaceDecoder struct {
+}
+
+func (decoder *emptyInterfaceDecoder) Decode(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+	nextValueType := iter.WhatIsNext()
+	switch nextValueType {
+	case jsoniter.ObjectValue:
+		obj := &DObject{}
+		iter.ReadVal(&obj.data)
+		*(*interface{})(ptr) = obj
+	case jsoniter.ArrayValue:
+		arr := &DArray{}
+		iter.ReadVal(&arr.data)
+		*(*interface{})(ptr) = arr
+	case jsoniter.StringValue:
+		*(*interface{})(ptr) = iter.ReadString()
+	case jsoniter.BoolValue:
+		*(*interface{})(ptr) = iter.ReadBool()
+	case jsoniter.NilValue:
+		iter.ReadNil()
+		*(*interface{})(ptr) = nil
+	case jsoniter.NumberValue:
+		number := iter.SkipAndReturnBytes()
+		subIter := json.BorrowIterator(number)
+		intVal := subIter.ReadInt64()
+		if subIter.Error == io.EOF {
+			json.ReturnIterator(subIter)
+			*(*interface{})(ptr) = intVal
+		} else {
+			json.ReturnIterator(subIter)
+			subIter = json.BorrowIterator(number)
+			*(*interface{})(ptr) = subIter.ReadFloat64()
+			if subIter.Error != nil && subIter.Error != io.EOF {
+				iter.ReportError("readNumber", subIter.Error.Error())
+			}
+			json.ReturnIterator(subIter)
+		}
+	default:
+		iter.ReportError("readEmptyInterface", fmt.Sprintf("unexpected value type: %v", nextValueType))
+	}
+}
+
+func Get(obj interface{}, path ...interface{}) interface{} {
+	for _, elem := range path {
+		switch typedElem := elem.(type) {
+		case int:
+			obj = obj.(*DArray).data[typedElem]
+		case string:
+			obj = obj.(*DObject).data[typedElem]
+		}
+	}
+	return obj
+}
---
+++ docstore/model/model_test.go
@@ -0,0 +1,37 @@
+package model
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_object(t *testing.T) {
+	should := require.New(t)
+	var obj interface{}
+	should.Nil(json.Unmarshal([]byte(`{"hello":"world"}`), &obj))
+	should.Equal("world", obj.(*DObject).data["hello"])
+}
+
+func Test_array(t *testing.T) {
+	should := require.New(t)
+	var obj interface{}
+	should.Nil(json.Unmarshal([]byte(`[1,2,3]`), &obj))
+	should.Equal(int64(1), obj.(*DArray).data[0])
+	should.Equal(int64(2), obj.(*DArray).data[1])
+	should.Equal(int64(3), obj.(*DArray).data[2])
+}
+
+func Test_float(t *testing.T) {
+	should := require.New(t)
+	var obj interface{}
+	should.Nil(json.Unmarshal([]byte(`1e+1`), &obj))
+	should.Equal(float64(10), obj.(float64))
+}
+
+func Test_nested(t *testing.T) {
+	should := require.New(t)
+	var obj interface{}
+	should.Nil(json.Unmarshal([]byte(`{"hello":[true, null]}`), &obj))
+	should.Equal(true, Get(obj, "hello", 0))
+	should.Equal(nil, Get(obj, "hello", 1))
+}
